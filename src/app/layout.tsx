bcaf4bb1d5e9a1370be7d1ce5b7a75915f81818f
DukeFPV <jcarloslorenzo@uoc.edu>
2024-12-17 20:38:25 +0100

    Refactor layout.tsx for improved readability and consistency; adjust formatting mobile FeatureCards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { NextUIProvider } from "@nextui-org/react";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { esES } from '@clerk/localizations'
-import Provider from "@/components/Provider";
-import { Toaster } from 'react-hot-toast'
-//import { UserButton } from "@clerk/nextjs";
-//import { auth, currentUser } from "@clerk/nextjs/server";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+// src/app/layout.tsx
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { NextUIProvider } from "@nextui-org/react"
+import "./globals.css"
+import { ClerkProvider } from "@clerk/nextjs"
+import { esES } from "@clerk/localizations"
+import Provider from "@/components/Provider"
+import { Toaster } from "react-hot-toast"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Sara IA",
   description: "Sara IA, tu asistente virtual",
-  icons: { icon: '/icons/favicon-24x24.png' }
-};
+  icons: { icon: "/icons/favicon-24x24.png" },
+}
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-
   return (
     <ClerkProvider localization={esES}>
       <Provider>
@@ -32,13 +31,13 @@ export default async function RootLayout({
           <body className="relative max-w-7xl m-auto">
             <NextUIProvider>
               <Header />
-                {children}
-                <Toaster />
+              {children}
+              <Toaster />
               <Footer />
             </NextUIProvider>
           </body>
         </html>
       </Provider>
     </ClerkProvider>
-  );
+  )
 }
