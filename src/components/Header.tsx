d2a9c1aa9467e63d2cee32d2fb09d5bb02f8e03a
DukeFPV <jcarloslorenzo@uoc.edu>
2024-12-18 01:00:24 +0100

    Refactor tailwind.config.ts for improved formatting and add homeMobile background image. Improvements UI responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,36 @@
-"use client";
+"use client"
 
-import { useState, ReactNode } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { IoIosArrowDown, IoMdMenu, IoMdClose } from 'react-icons/io';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
-import { Switch } from "@nextui-org/react";
-import { MoonIcon } from "../components/MoonIcon";
-import { SunIcon } from "../components/SunIcon";
-import { usePathname } from 'next/navigation';
-import { UserButton, useUser } from "@clerk/nextjs";
-import { useClerk } from '@clerk/nextjs'
+import { useState, ReactNode } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { IoIosArrowDown, IoMdMenu, IoMdClose } from "react-icons/io"
+import { useAutoAnimate } from "@formkit/auto-animate/react"
+import { Switch } from "@nextui-org/react"
+import { MoonIcon } from "../components/MoonIcon"
+import { SunIcon } from "../components/SunIcon"
+import { usePathname } from "next/navigation"
+import { UserButton, useUser } from "@clerk/nextjs"
+import { useClerk } from "@clerk/nextjs"
 
 // Icons
-import microIcon from "/public/icons/microphone.svg";
-import microVoice from "/public/icons/voice.svg";
-import microData from "/public/icons/data.svg";
-import microGoals from "/public/icons/goals.svg";
-import microUi from "/public/icons/browser_ui.svg";
+import microIcon from "/public/icons/microphone.svg"
+import microVoice from "/public/icons/voice.svg"
+import microData from "/public/icons/data.svg"
+import microGoals from "/public/icons/goals.svg"
+import microUi from "/public/icons/browser_ui.svg"
 
 type NavItem = {
-  label: string;
-  link?: string;
-  children?: NavItem[];
-  iconImage?: string;
-};
+  label: string
+  link?: string
+  children?: NavItem[]
+  iconImage?: string
+}
 
 const navItems: NavItem[] = [
   { label: "Que es SARA", link: "/nosotros" },
   { label: "Contacto", link: "/contacto" },
   { label: "Quienes somos", link: "/nosotros" },
-];
+]
 
 const protectedNavItems: NavItem[] = [
   {
@@ -38,123 +38,162 @@ const protectedNavItems: NavItem[] = [
     link: "/sara-ia",
   },
   {
-    label: 'Ajustes',
-    link: '#',
+    label: "Ajustes",
+    link: "#",
     children: [
-      { label: "Interfaz", link: "#", iconImage: microUi, children: [{ label: "Sugerencias", iconImage: "", link: "#" }, { label: "Modo Oscuro", iconImage: "", link: "#" }] },
+      {
+        label: "Interfaz",
+        link: "#",
+        iconImage: microUi,
+        children: [
+          { label: "Sugerencias", iconImage: "", link: "#" },
+          { label: "Modo Oscuro", iconImage: "", link: "#" },
+        ],
+      },
       { label: "Micrófono", link: "#", iconImage: microIcon },
       { label: "Voz de Sara", link: "#", iconImage: microVoice },
       { label: "Mis Datos", link: "#", iconImage: microData },
       { label: "Mis Logros", link: "#", iconImage: microGoals },
-    ]
+    ],
   },
-];
+]
 
 export default function Navbar() {
-  const { isSignedIn, user } = useUser();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [animationParent] = useAutoAnimate();
+  const { isSignedIn, user } = useUser()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [animationParent] = useAutoAnimate()
 
-  const pathname = usePathname();
-  const isHomePage = pathname === "/";
+  const pathname = usePathname()
+  const isHomePage = pathname === "/"
 
   function openMenu() {
-    setIsMenuOpen(true);
+    setIsMenuOpen(true)
   }
 
   function closeMenu() {
-    setIsMenuOpen(false);
+    setIsMenuOpen(false)
   }
 
   const { signOut } = useClerk()
 
   return (
-    <nav className={`z-50 mr-auto flex max-w-7xl justify-between px-4 py-5 text-sm ${isHomePage ? 'text-neutral-200' : 'text-neutral-800 hover:text-purple-700'}`}>
-      <section ref={animationParent} className='flex items-center gap-10'>
+    <nav
+      className={`z-50 mr-auto flex max-w-7xl justify-between px-4 py-5 text-sm ${isHomePage ? "text-neutral-200" : "text-neutral-800 hover:text-purple-700"}`}
+    >
+      <section ref={animationParent} className="flex items-center gap-10">
         <Link href="/">
-          <Image src={`${isHomePage ?'/icons/logo3.svg' : '/icons/logo2.svg'}`} className='shadow-purple-200' width={150} height={50} alt='logo' />
+          <Image
+            src={`${isHomePage ? "/icons/logo3.svg" : "/icons/logo2.svg"}`}
+            className="shadow-purple-200"
+            width={150}
+            height={50}
+            alt="logo"
+          />
         </Link>
         {isMenuOpen && <MobileNav closeMenu={closeMenu} />}
-        <div className={`hidden md:flex items-center gap-4 transition-all ${isHomePage ? 'text-neutral-200' : 'text-neutral-800 '}`}>
+        <div
+          className={`hidden lg:flex items-center gap-4 transition-all ${isHomePage ? "text-neutral-200" : "text-neutral-800 "}`}
+        >
           {navItems.map((d, i) => (
-            <span key={i} className={`relative group px-2 py-3 transition-all ${isHomePage ? 'text-neutral-200 hover:text-purple-400' : 'text-neutral-800 hover:text-purple-500'}`}>
+            <span
+              key={i}
+              className={`relative px-2 py-3 transition-all ${isHomePage ? "text-neutral-200 hover:text-purple-400" : "text-neutral-800 hover:text-purple-500"}`}
+            >
               <Link href={d.link ?? "#"}>
-                <p className='flex cursor-pointer items-center gap-2 '>
+                <p className="flex cursor-pointer items-center gap-2 ">
                   <span>{d.label}</span>
                 </p>
               </Link>
             </span>
           ))}
-          {isSignedIn && protectedNavItems.map((d, i) => (
-            <span key={i} className={`relative group px-2 py-3 transition-all ${isHomePage ? 'text-neutral-200 hover:text-purple-400' : 'text-neutral-800 hover:text-purple-500'}`}>
-              <Link href={d.link ?? "#"}>
-                <p className='flex cursor-pointer items-center gap-2 '>
-                  <span>{d.label}</span>
-                  {d.children && (
-                    <IoIosArrowDown className='rotate-180 transition-all group-hover:rotate-0' />
-                  )}
-                </p>
-              </Link>
-              {d.children && (
-                <div className="z-50 absolute right-0 top-10 hidden w-auto flex-col gap-1 rounded-lg bg-white py-3 shadow-md transition-all group-hover:flex">
-                  {d.children.map((c, i) => (
-                    <Link key={i} href={c.link ?? "#"} className="flex cursor-pointer items-center py-1 pl-6 pr-8 text-neutral-800 hover:text-purple-500">
-                      {c.iconImage && (
-                        <Image src={c.iconImage} alt="item-icon" />
-                      )}
-                      <span className="whitespace-nowrap pl-3">
-                        {c.label}
-                      </span>
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </span>
-          ))}
+          {isSignedIn &&
+            protectedNavItems.map((d, i) => (
+              <span
+                key={i}
+                className={`relative group px-2 py-3 transition-all ${isHomePage ? "text-neutral-200 hover:text-purple-400" : "text-neutral-800 hover:text-purple-500"}`}
+              >
+                <Link href={d.link ?? "#"}>
+                  <p className="flex cursor-pointer items-center gap-2 ">
+                    <span>{d.label}</span>
+                    {d.children && (
+                      <IoIosArrowDown className="rotate-180 transition-all group-hover:rotate-0" />
+                    )}
+                  </p>
+                </Link>
+                {d.children && (
+                  <div className="z-50 absolute right-0 top-10 hidden w-auto flex-col gap-1 rounded-lg bg-white py-3 shadow-md transition-all group-hover:flex">
+                    {d.children.map((c, i) => (
+                      <Link
+                        key={i}
+                        href={c.link ?? "#"}
+                        className="flex cursor-pointer items-center py-1 pl-6 pr-8 text-neutral-800 hover:text-purple-500"
+                      >
+                        {c.iconImage && (
+                          <Image src={c.iconImage} alt="item-icon" />
+                        )}
+                        <span className="whitespace-nowrap pl-3">
+                          {c.label}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </span>
+            ))}
         </div>
       </section>
-      <section className='hidden md:flex items-center gap-8'>
+      <section className="hidden lg:flex items-center gap-8">
         {isSignedIn ? (
-          <div className='flex items-center gap-4'>
+          <div className="flex items-center gap-4">
             <UserButton afterSwitchSessionUrl="/" />
-              <span className={` ${isHomePage ? 'text-white text-nowrap' : 'text-neutral-800 text-nowrap hover:text-purple-600'}`}>
-                Hola!👋 {user?.firstName}
-              </span>
-              <button 
-                className={`w-full max-w-[180px] border-2 px-3 py-2 rounded-full transition-all ${isHomePage ? 'text-neutral-400 border-red-300 hover:border-red-600 hover:text-white/90' : 'text-black hover:text-red-500 border-red-300 hover:border-red-600'}`} 
-                onClick={() => signOut({ redirectUrl: '/'})}
-              >
-                Desconectar
-              </button>
-                
+            <span
+              className={` ${isHomePage ? "text-white text-nowrap" : "text-neutral-800 text-nowrap hover:text-purple-600"}`}
+            >
+              Hola!👋 {user?.firstName}
+            </span>
+            <button
+              className={`w-full max-w-[180px] border-2 px-3 py-2 rounded-full transition-all ${isHomePage ? "text-neutral-400 border-red-300 hover:border-red-600 hover:text-white/90" : "text-black hover:text-red-500 border-red-300 hover:border-red-600"}`}
+              onClick={() => signOut({ redirectUrl: "/" })}
+            >
+              Desconectar
+            </button>
           </div>
         ) : (
           <>
-            <Link href="/sign-in" className={`w-28 text-center border-purple-300 border-2 px-3 py-2 rounded-full transition-all hover:border-purple-600 hover:text-purple-700/90 text-nowrap ${isHomePage ? 'text-neutral-400' : 'text-neutral-600 hover:text-cyan-500'}`}>
+            <Link
+              href="/sign-in"
+              className={`w-28 text-center border-purple-300 border-2 px-3 py-2 rounded-full transition-all hover:border-purple-600 hover:text-purple-700/90 text-nowrap ${isHomePage ? "text-neutral-400" : "text-neutral-600 hover:text-cyan-500"}`}
+            >
               Entrar
             </Link>
-            <Link href="/sign-up" className={`w-28 text-center text-neutral-400 border-cyan-300 border-2 px-3 py-2 rounded-full transition-all hover:border-cyan-700 hover:text-cyan-700/90 ${isHomePage ? 'text-neutral-400' : 'text-neutral-600 hover:text-cyan-500'}`}>
+            <Link
+              href="/sign-up"
+              className={`w-28 text-center text-neutral-400 border-cyan-300 border-2 px-3 py-2 rounded-full transition-all hover:border-cyan-700 hover:text-cyan-700/90 ${isHomePage ? "text-neutral-400" : "text-neutral-600 hover:text-cyan-500"}`}
+            >
               Registro
             </Link>
           </>
         )}
       </section>
-      <IoMdMenu onClick={openMenu} className='cursor-pointer text-4xl md:hidden text-black' />
+      <IoMdMenu
+        onClick={openMenu}
+        className={`cursor-pointer text-4xl lg:hidden ${isHomePage ? "text-white" : "text-black"}`}
+      />
     </nav>
-  );
+  )
 }
 
 function MobileNav({ closeMenu }: { closeMenu: () => void }) {
-  const { isSignedIn, user } = useUser();
-  const ajustesItem = protectedNavItems.find(item => item.label === "Ajustes");
+  const { isSignedIn, user } = useUser()
+  const ajustesItem = protectedNavItems.find((item) => item.label === "Ajustes")
 
   return (
-    <div className='fixed left-0 top-0 flex h-full w-full justify-end bg-black/60 md:hidden z-50'>
-      <div className='h-full w-[65%] bg-white px-4 py-4 justify-between z-50'>
-        <figure className='flex justify-end'>
-          <IoMdClose onClick={closeMenu} className='cursor-pointer text-4xl' />
+    <div className="fixed left-0 top-0 flex h-full w-full justify-end bg-black/60 lg:hidden z-50">
+      <div className="h-full w-[65%] bg-white px-4 py-4 justify-between z-50">
+        <figure className="flex justify-end">
+          <IoMdClose onClick={closeMenu} className="cursor-pointer text-4xl" />
         </figure>
-        <div className='flex flex-col gap-4 transition-all items-start z-50'>
+        <div className="flex flex-col gap-4 transition-all items-start z-50">
           {navItems.map((d, i) => (
             <Link key={i} href={d.link ?? "#"}>
               <span className="flex cursor-pointer items-center py-1 pl-6 pr-8 text-neutral-400 hover:text-black">
@@ -172,7 +211,11 @@ function MobileNav({ closeMenu }: { closeMenu: () => void }) {
               {ajustesItem?.children?.map((c, i) => (
                 <SingleNavItem key={i} label={c.label} iconImage={c.iconImage}>
                   {c.children?.map((child, index) => (
-                    <SingleNavItem key={index} label={child.label} iconImage={child.iconImage} />
+                    <SingleNavItem
+                      key={index}
+                      label={child.label}
+                      iconImage={child.iconImage}
+                    />
                   ))}
                 </SingleNavItem>
               ))}
@@ -180,52 +223,76 @@ function MobileNav({ closeMenu }: { closeMenu: () => void }) {
           )}
         </div>
         {isSignedIn ? (
-          <section className='flex flex-col items-center gap-8 mt-40'>
-            <Link href="/" className='w-full max-w-[180px]'>
-              <button className='w-full rounded-xl bg-black border-neutral-100 text-neutral-100 transition-all border-2 px-4 py-2 hover:border-purple-400 hover:text-white/90'>
+          <section className="flex flex-col items-center gap-8 mt-40">
+            <Link href="/" className="w-full max-w-[180px]">
+              <button className="w-full rounded-xl bg-black border-neutral-100 text-neutral-100 transition-all border-2 px-4 py-2 hover:border-purple-400 hover:text-white/90">
                 Cerrar Aplicación
               </button>
             </Link>
-            <hr className='w-full border-b-purple-950 border-2' />
-            <div className='flex flex-col items-center gap-4'>
-              <span className='text-neutral-800'>Hola!👋 {user?.firstName}</span>
-              <Link href="/sign-out" className='text-center w-full max-w-[180px] text-neutral-400 border-cyan-600 border-2 px-3 py-2 rounded-full transition-all hover:border-cyan-300 hover:text-white/90'>
+            <hr className="w-full border-b-purple-950 border-2" />
+            <div className="flex flex-col items-center gap-4">
+              <span className="text-neutral-800">
+                Hola!👋 {user?.firstName}
+              </span>
+              <Link
+                href="/sign-out"
+                className="text-center w-full max-w-[180px] text-neutral-400 border-cyan-600 border-2 px-3 py-2 rounded-full transition-all hover:border-cyan-300 hover:text-white/90"
+              >
                 Desconectar
               </Link>
             </div>
           </section>
         ) : (
-          <section className='flex flex-col items-center gap-8 mt-40'>
-            <Link href="/sign-in" className='text-center w-full max-w-[180px] text-neutral-400 border-purple-800 border-2 px-3 py-2 rounded-full transition-all hover:text-black/90 mt-10'>
+          <section className="flex flex-col items-center gap-8 mt-40">
+            <Link
+              href="/sign-in"
+              className="text-center w-full max-w-[180px] text-neutral-400 border-purple-800 border-2 px-3 py-2 rounded-full transition-all hover:text-black/90 mt-10"
+            >
               Entrar
             </Link>
-            <Link href="/sign-up" className='text-center w-full max-w-[180px] text-neutral-400 border-cyan-600 border-2 px-3 py-2 rounded-full transition-all hover:text-black/90'>
+            <Link
+              href="/sign-up"
+              className="text-center w-full max-w-[180px] text-neutral-400 border-cyan-600 border-2 px-3 py-2 rounded-full transition-all hover:text-black/90"
+            >
               Registro
             </Link>
           </section>
         )}
       </div>
     </div>
-  );
+  )
 }
 
-function SingleNavItem({ label, iconImage, children }: { label: string, iconImage?: string, children?: ReactNode }) {
-  const [animationParent] = useAutoAnimate();
-  const [isOpen, setIsOpen] = useState(false);
+function SingleNavItem({
+  label,
+  iconImage,
+  children,
+}: {
+  label: string
+  iconImage?: string
+  children?: ReactNode
+}) {
+  const [animationParent] = useAutoAnimate()
+  const [isOpen, setIsOpen] = useState(false)
 
   function toggleItem() {
-    setIsOpen(!isOpen);
+    setIsOpen(!isOpen)
   }
 
   return (
-    <div ref={animationParent} className='relative px-2 py-3 transition-all'>
-      <div onClick={toggleItem} className='relative flex cursor-pointer items-center gap-2 text-neutral-400 hover:text-black'>
+    <div ref={animationParent} className="relative px-2 py-3 transition-all">
+      <div
+        onClick={toggleItem}
+        className="relative flex cursor-pointer items-center gap-2 text-neutral-400 hover:text-black"
+      >
         {iconImage && (
           <Image src={iconImage} alt="item-icon" width={20} height={20} />
         )}
         <span>{label}</span>
         {children && (
-          <IoIosArrowDown className={`ml-auto mr-5 transition-transform ${isOpen ? "rotate-180" : ""}`} />
+          <IoIosArrowDown
+            className={`ml-auto mr-5 transition-transform ${isOpen ? "rotate-180" : ""}`}
+          />
         )}
       </div>
       {isOpen && children && (
@@ -234,5 +301,5 @@ function SingleNavItem({ label, iconImage, children }: { label: string, iconImag
         </div>
       )}
     </div>
-  );
+  )
 }
